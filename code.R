e3a505e537162aa23a396b9581542f5ce3dcb514
John <wongthjohn@gmail.com>
2025-04-28 04:27:31 -0400

    finished slides

--- code.R
+++ code.R
@@ -3,13 +3,14 @@ set.seed(1000)
 
 # Parameters -------------------------------------------------------------------
 i_max = 50
-t_max = 100
+t_max = 1000
 alpha = 0.5
 delta = 0.2
 beta_0 = 0.3
 beta_1 = -0.4
 gamma_1 = 0.6
 theta_1 = 0.7
+
 
 # Generate panel ---------------------------------------------------------------
 
@@ -126,10 +127,11 @@ ivreg::ivreg(
 ) %>% summary()
 # this is getting closer.
 
-ivreg::ivreg(
+tsls_dl1_as_control <- ivreg::ivreg(
   formula = y ~ d + d_l1 | z + d_l1,
   data = df_panel
-) %>% summary()
+)
+summary(tsls_dl1_as_control)
 # oh this is correctly estimating.
 
 ivreg::ivreg(
@@ -137,6 +139,36 @@ ivreg::ivreg(
   data = df_panel
 ) %>% summary()
 # using d_l1 on d_l2 also works.
+
+# Tables -----------------------------------------------------------------------
+true_model <- texreg::createTexreg(
+  coef.names = c("(Intercept)", "d", "d_l1"),
+  coef = c(0, beta_0, beta_1)
+)
+
+table_omitted <- texreg::texreg(
+  l = list(true_model, tsls_d),
+  custom.model.names = c("True", "TSLS"),
+  custom.coef.names = c(
+    "Intercept",
+    "$d_t$",
+    "$Ld_t$"
+  ),
+  include.rsquared = FALSE,
+  include.adjrs = FALSE
+)
+
+table_lagd <- texreg::texreg(
+  l = list(true_model, tsls_d, tsls_dl1_as_control),
+  custom.model.names = c("True", "TSLS", "TSLS With Lag"),
+  custom.coef.names = c(
+    "Intercept",
+    "$d_t$",
+    "$Ld_t$"
+  ),
+  include.rsquared = FALSE,
+  include.adjrs = FALSE
+)
 
 # Test with IRFs ---------------------------------------------------------------
 delta_hat = lm_stage1$coefficients["log(z)"]
@@ -225,15 +257,50 @@ summary(lm_reduced_form_lagy)
 delta*beta_0
 
 # intentionally omit the lagged terms
-ivreg::ivreg(
+tsls_solow_omitted_stage2 <- ivreg::ivreg(
   formula = y ~ d | z,
   data = df_panel_lagy
-) %>% summary()
+) 
+
+tsls_solow_omitted_stage1 <- lm(
+  formula = d ~ z,
+  data = df_panel_lagy
+) 
 # the coefficient becomes negative
 
-# generating y and d simultaneously --------------------------------------------
-# i should know the first y_{t-1} = 0. the subsequent y_{t-1} are known. 
-# generate d_t (z_t) first.
+# table ------------------------------------------------------------------------
+true_solow_model_stage1 <- texreg::createTexreg(
+  coef.names = c("(Intercept)", "z", "d_l1", "y_l1"),
+  coef = c(0, delta, alpha, theta_1)
+)
+
+true_solow_model_stage2 <- texreg::createTexreg(
+  coef.names = c("(Intercept)", "d", "d_l1", "y_l1"),
+  coef = c(0, beta_0, beta_1, gamma_1)
+)
+
+table_solow <- texreg::texreg(
+  l = list(
+    true_solow_model_stage1, 
+    tsls_solow_omitted_stage1,
+    lm_stage1_lagy,
+    true_solow_model_stage2,
+    tsls_solow_omitted_stage2,
+    tsls_lagy
+    ),
+  custom.header = list("1st-Stage ($d_t$)" = 1:3, "2nd-Stage ($y_t$)" = 4:6),
+  custom.model.names = c("True", "(a)", "(b)", "True", "(c)", "(d)"),
+  custom.coef.names = c(
+    "Intercept",
+    "$z_t$",
+    "$Ld_t$",
+    "$Ly_t$",
+    "$d_t$"
+  ),
+  include.rsquared = FALSE,
+  include.adjrs = FALSE,
+  no.margin = TRUE
+)
 
 # monte-carlo analysis ---------------------------------------------------------
 mc_coef = read.csv("mc_results.csv")
@@ -251,15 +318,30 @@ plot_density <- function(x){
   # Plot
   ggplot(dens_df, aes(x = x, y = y)) +
     # Shade the ±1 SD region
-    geom_area(data = subset(dens_df, x >= mu - sigma & x <= mu + sigma),
+    geom_area(data = subset(dens_df, x >= mu - 2*sigma & x <= mu + 2*sigma),
               fill = "skyblue", alpha = 0.5) +
     # Full density curve
     geom_line(color = "black", linewidth = 0.5)+
-    # Add vertical lines for mean and ±1 SD
-    # geom_vline(xintercept = beta_0, color = "black", linetype = "dashed")  +
-    labs(title = "Density Plot with ±1 SD Shaded",
-         x = "Value", y = "Density")
+    # Add vertical lines for mean and ±2 SD
+    geom_vline(xintercept = beta_0, color = "black", linetype = "dashed")  +
+    labs(title = "",
+         x = "Estimated Coefficient", y = "Density") +
+    theme(
+      axis.title = element_text(size = 20),
+      axis.text = element_text(size = 18)
+    )
 }
 
-plot_density(mc_coef$y_l1)
+plot_unbiased = plot_density(mc_coef$d)
 
+mc_coef_biased = read.csv("mc_results_biased.csv")
+plot_biased = plot_density(mc_coef_biased$d)
+
+mc_coef_labreque = read.csv("mc_results_labrecque.csv")
+plot_labrecque = plot_density(mc_coef_labreque$d)
+
+
+
+# generating y and d simultaneously --------------------------------------------
+# i should know the first y_{t-1} = 0. the subsequent y_{t-1} are known. 
+# generate d_t (z_t) first.
--- run_monte_carlo.R
+++ run_monte_carlo.R
@@ -1,6 +1,9 @@
 source("code.R")
-
+# parameters
 m_max = 500
+t_max = 100
+
+# one instance
 mc_results = matrix(rep(NA, m_max*4), ncol = 4)
 set.seed(1000)
 for(m in 1:m_max){
@@ -13,3 +16,30 @@ for(m in 1:m_max){
 }
 colnames(mc_results) = c("intercept", "d", "d_l1", "y_l1")
 write.csv(mc_results, "mc_results.csv")
+
+# biased example
+mc_results = matrix(rep(NA, m_max*2), ncol = 2)
+for(m in 1:m_max){
+  df = gen_solow_panel()
+  tsls = ivreg::ivreg(
+    formula = y ~ d | z,
+    data = df
+  )
+  mc_results[m,] = tsls$coefficients
+}
+colnames(mc_results) = c("intercept", "d")
+write.csv(mc_results, "mc_results_biased.csv")
+
+# lagged treatment only
+mc_results = matrix(rep(NA, m_max*3), ncol = 3)
+set.seed(1000)
+for(m in 1:m_max){
+  df = gen_labrecque_panel()
+  tsls = ivreg::ivreg(
+    formula = y ~ d + d_l1 | z + d_l1,
+    data = df
+  )
+  mc_results[m,] = tsls$coefficients
+}
+colnames(mc_results) = c("intercept", "d", "d_l1")
+write.csv(mc_results, "mc_results_labrecque.csv")
